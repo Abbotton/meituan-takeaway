9f9e4712d23841e5910ca93f0f97798e50f56c4d
Abbotton <Abbotton@users.noreply.github.com>
2021-01-12 03:09:05 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Request/BaseRequest.php
+++ src/Request/BaseRequest.php
@@ -10,100 +10,107 @@ class BaseRequest
     protected $guzzleOptions = [];
     private $config;
     private $log;
-    
+
     public function __construct(Config $config)
     {
         $this->config = $config;
         $this->log = $config->getLogger();
     }
-    
+
     protected function get($action, array $options = [])
     {
-        $params_head = array(
+        $params_head = [
             'timestamp' => time(),
-            'app_id' => $this->config->app_id,
-        );
-        
+            'app_id'    => $this->config->app_id,
+        ];
+
         $options = array_merge($params_head, $options);
-        $sig = $this->generateSignature($this->config->request_url . $action, $options);
+        $sig = $this->generateSignature($this->config->request_url.$action, $options);
         $options['sig'] = $sig;
-        
-        $url = $this->config->request_url . $action;
-        
+
+        $url = $this->config->request_url.$action;
+
         return $this->request('GET', $url, ['query' => $options]);
     }
-    
+
     private function generateSignature($action, $params)
     {
         $params = $this->concatParams($params);
-        $str = $action . '?' . $params . $this->config->app_secret;
+        $str = $action.'?'.$params.$this->config->app_secret;
+
         return md5($str);
     }
-    
+
     private function concatParams($params)
     {
         ksort($params);
-        $pairs = array();
+        $pairs = [];
         foreach ($params as $key => $val) {
-            array_push($pairs, $key . '=' . $val);
+            array_push($pairs, $key.'='.$val);
         }
+
         return join('&', $pairs);
     }
-    
+
     private function request($method, $url, $options = [])
     {
         $log_id = $this->createRequestId();
-        
+
         $method = strtoupper($method);
-    
+
         if ($this->config->log_path) {
             $this->log->debug('API Request:', compact('log_id', 'url', 'method', 'options'));
         }
-        
+
         $response = $this->getHttpClient()->request($method, $url, $options);
         if ($this->config->log_path) {
             $this->log->debug('API Response:', [
-                'log_id' => $log_id,
-                'Status' => $response->getStatusCode(),
-                'Reason' => $response->getReasonPhrase(),
+                'log_id'  => $log_id,
+                'Status'  => $response->getStatusCode(),
+                'Reason'  => $response->getReasonPhrase(),
                 'Headers' => $response->getHeaders(),
-                'Body' => strval($response->getBody()),
+                'Body'    => strval($response->getBody()),
             ]);
         }
+
         return $response->getBody();
     }
-    
+
     private function createRequestId()
     {
-        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+        return sprintf(
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
             mt_rand(0, 0xffff),
             mt_rand(0, 0x0fff) | 0x4000,
             mt_rand(0, 0x3fff) | 0x8000,
-            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff)
         );
     }
-    
+
     public function getHttpClient()
     {
         return new Client($this->guzzleOptions);
     }
-    
+
     protected function post($action, array $params = [], string $enctype = 'form_params')
     {
-        $params_head = array(
+        $params_head = [
             'timestamp' => time(),
-            'app_id' => $this->config->app_id,
-        );
-        
+            'app_id'    => $this->config->app_id,
+        ];
+
         $params = array_merge($params_head, $params);
-        $url = $this->config->request_url . $action;
+        $url = $this->config->request_url.$action;
         $sig = $this->generateSignature($url, $params);
         $params['sig'] = $sig;
-        
+
         return $this->request('POST', $url, [$enctype => $params]);
     }
-    
+
     public function setGuzzleOptions(array $options)
     {
         $this->guzzleOptions = $options;
--- src/Request/Comment.php
+++ src/Request/Comment.php
@@ -7,31 +7,34 @@ use Psr\Http\Message\StreamInterface;
 class Comment extends BaseRequest
 {
     /**
-     * 根据门店id批量查询评价信息
+     * 根据门店id批量查询评价信息.
      *
      * @param array $params
+     *
      * @return StreamInterface
      */
     public function query(array $params)
     {
         return $this->get('comment/query', $params);
     }
-    
+
     /**
-     * 根据评价id添加商家回复
+     * 根据评价id添加商家回复.
      *
      * @param array $params
+     *
      * @return StreamInterface
      */
     public function addReply(array $params)
     {
         return $this->post('poi/comment/add_reply', $params);
     }
-    
+
     /**
-     * 通过门店ID获取当前门店评分
+     * 通过门店ID获取当前门店评分.
      *
      * @param array $params
+     *
      * @return StreamInterface
      */
     public function score(array $params)
